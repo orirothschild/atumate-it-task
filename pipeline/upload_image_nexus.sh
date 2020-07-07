11fa910831d08309157830e12841b0f60039e04e
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-07 19:41:56 +0000

    again

--- pipeline/upload_image_nexus.sh
+++ pipeline/upload_image_nexus.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+NexusDockerRegistryUrl="ada3296ebd44a49bf9ca6d98a0fe4cfa-1364365130.us-east-1.elb.amazonaws.com:8083"
+
+echo "$NEXUS_PASS" | docker login -u "$NEXUS_ID" --password-stdin "$NexusDockerRegistryUrl"
