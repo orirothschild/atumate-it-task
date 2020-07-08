087f066a2635179aad7ecbd087965099b20795f4
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-08 08:03:55 +0000

    now

--- pipeline/upload_image_nexus.sh
+++ pipeline/upload_image_nexus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-NexusDockerRegistryUrl="ada3296ebd44a49bf9ca6d98a0fe4cfa-1364365130.us-east-1.elb.amazonaws.com:8083"
+NexusDockerRegistryUrl="http://ada3296ebd44a49bf9ca6d98a0fe4cfa-1364365130.us-east-1.elb.amazonaws.com:5000/repository/automate-it-docker/"
 
-echo "$NEXUS_PASS" | docker login -u "$NEXUS_ID" --password-stdin "$NexusDockerRegistryUrl"
+docker login "$NexusDockerRegistryUrl"
