4d6259ec6bef73a6bdc9e4a7a8eb2d99f130567b
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2020-07-08 09:50:28 +0000

    now

--- pipeline/sonarqube-test.sh
+++ pipeline/sonarqube-test.sh
@@ -6,5 +6,5 @@ echo "*****************************"
 
 mvn sonar:sonar \
 -Dsonar.projectKey=automate-it \
--Dsonar.host.url=http://a573d165da94f409f8d6b90ae8ba0aae-1623065677.us-east-1.elb.amazonaws.com:9000 \
+-Dsonar.host.url=http://a607504ba8e0c4ab48628792918b149b-144520793.us-east-1.elb.amazonaws.com:9000 \
 -Dsonar.login="$SONARQUBE_TOKEN"
